4d81d7e2867f42f5712d2c8fe11a71f30ce4f021
Samir <samiratef689@gmail.com>
2025-03-05 15:35:01 +0200

    adding sound to the item

--- lib/components/item.dart
+++ lib/components/item.dart
@@ -1,9 +1,11 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 import '../models/number.dart';
+
 class Item extends StatelessWidget {
   const Item({Key? key, required this.number}) : super(key: key);
-  final Number number ;
+  final Number number;
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -19,7 +21,8 @@ class Item extends StatelessWidget {
           ),
           Padding(
             padding: const EdgeInsets.only(left: 16),
-            child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
               Text(
                 number.jpName,
                 style: TextStyle(color: Colors.white, fontSize: 18),
@@ -33,15 +36,17 @@ class Item extends StatelessWidget {
           Spacer(
             flex: 1,
           ),
-          Padding(
-            padding: const EdgeInsets.only(right: 16),
-            child: Icon(
+          IconButton(
+            onPressed: () {
+              final player = AudioPlayer();
+              player.play(AssetSource(number.sound));
+            },
+            icon: Icon(
               Icons.play_arrow,
               color: Colors.white,
               size: 30,
             ),
           ),
-
         ],
       ),
     );
--- lib/screens/numbers_page.dart
+++ lib/screens/numbers_page.dart
@@ -6,42 +6,52 @@ class NumbersPage extends StatelessWidget {
   const NumbersPage({Key? key}) : super(key: key);
   final List<Number> numbers = const [
     Number(
+        sound: 'sounds/numbers/number_one_sound.mp3',
         image: 'assets/images/numbers/number_one.png',
         jpName: 'ichi',
         enName: 'one'),
     Number(
+        sound: 'sounds/numbers/number_two_sound.mp3',
         image: 'assets/images/numbers/number_two.png',
         jpName: 'Ni',
         enName: 'two'),
     Number(
+        sound: 'sounds/numbers/number_three_sound.mp3',
         image: 'assets/images/numbers/number_three.png',
         jpName: 'San',
         enName: 'three'),
     Number(
+        sound: 'sounds/numbers/number_four_sound.mp3',
         image: 'assets/images/numbers/number_four.png',
         jpName: 'Shi',
         enName: 'four'),
     Number(
+        sound: 'sounds/numbers/number_five_sound.mp3',
         image: 'assets/images/numbers/number_five.png',
         jpName: 'Go',
         enName: 'five'),
     Number(
+        sound: 'sounds/numbers/number_six_sound.mp3',
         image: 'assets/images/numbers/number_six.png',
         jpName: 'Roku',
         enName: 'six'),
     Number(
+        sound: 'sounds/numbers/number_seven_sound.mp3',
         image: 'assets/images/numbers/number_seven.png',
         jpName: 'Sebun',
         enName: 'seven'),
     Number(
+        sound: 'sounds/numbers/number_eight_sound.mp3',
         image: 'assets/images/numbers/number_eight.png',
         jpName: 'hichi',
         enName: 'eight'),
     Number(
+        sound: 'sounds/numbers/number_nine_sound.mp3',
         image: 'assets/images/numbers/number_nine.png',
         jpName: 'Kyu',
         enName: 'nine'),
     Number(
+        sound: 'sounds/numbers/number_ten_sound.mp3',
         image: 'assets/images/numbers/number_ten.png',
         jpName: 'Ju',
         enName: 'ten'),
